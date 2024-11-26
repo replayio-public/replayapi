fdba164186254477d6552a765595d40f9f88f536
D. Seifert <Domiii@users.noreply.github.com>
2024-11-26 18:52:05 +0800

    comment

--- testing/globalTeardown.js
+++ testing/globalTeardown.js
@@ -1,5 +1,5 @@
 module.exports = function globalTeardown() {
   // TODO: This does not work. For some reason, we don't get `window` in this context, despite `bootstrap.ts` setting it up.
-  // NOTE: This is a debug functionality added in devtools to close the session socket.
+  // NOTE: `disconnect` is a debug utility added in `devtools`. It should close the session socket.
   globalThis.window?.disconnect?.();
 }
