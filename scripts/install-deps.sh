70a32471daa55327b31961753f713218e7abc37d
D. Seifert <Domiii@users.noreply.github.com>
2024-12-09 05:16:17 +0800

    Also run `yarn` on devtools on init

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 set -ex
 
-cd "$(dirname "$0")"/..
+REPLAY_DIR="$(dirname "$0")/../.."
+
+# Clone devtools if not already present.
+if [ ! -d "$REPLAY_DIR/devtools" ]; then
+    git -C "$REPLAY_DIR/devtools" clone https://github.com/replayio/devtools.git
+fi
+cd "$REPLAY_DIR/devtools"
+# nvm use
+yarn install
+
+# Init replayapi.
+cd "$REPLAY_DIR/replayapi"
 
 export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
 corepack enable
 yarn
-
-# Clone devtools if not already present.
-if [ ! -d "../devtools" ]; then
-    git -C .. clone https://github.com/replayio/devtools.git
-fi
 
 export REPLAY_DIR=$(pwd)/..
 yarn yalc-all
