a5d2c15bf83aae41be41f47af1a5d652e70de070
Chris Toshok <toshok@gmail.com>
2024-12-18 09:48:05 -0800

    ensure REPLAY_DIR is an absolute directory, and remove the redundant setting of the variable

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -ex
 
-REPLAY_DIR="$(dirname "$0")/../.."
+REPLAY_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
 
 # Clone devtools if not already present.
 if [ ! -d "$REPLAY_DIR/devtools" ]; then
     git clone https://github.com/replayio/devtools.git "$REPLAY_DIR/devtools"
 fi
+
 cd "$REPLAY_DIR/devtools"
 # nvm use
 yarn install
@@ -18,5 +19,4 @@ export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
 corepack enable
 yarn
 
-export REPLAY_DIR=$(pwd)/..
 yarn yalc-all
