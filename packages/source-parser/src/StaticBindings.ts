9f07ed6a7939642a9e6f7e9aab1256008e14aade
D. Seifert <Domiii@users.noreply.github.com>
2025-01-05 22:51:23 +0800

    comment

--- packages/source-parser/src/StaticBindings.ts
+++ packages/source-parser/src/StaticBindings.ts
@@ -2,7 +2,7 @@ import { CodeAtLocation } from "@replayio/data/src/recordingData/types";
 
 export type StaticBinding = {
   /**
-   * Binding kind, based on Babel terminology.
+   * Binding kind, based on Babel: var, let, const, param etc.
    * @see https://github.com/babel/babel/blob/main/packages/babel-traverse/src/scope/binding.ts#L5
    */
   kind: string;
